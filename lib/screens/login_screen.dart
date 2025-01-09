1b6c87a95415a075aa97481eb0ed4ee38d4132ae
Isma <shadqi1890@gmail.com>
2025-01-09 17:08:13 +0700

    Login Page

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(Icons.arrow_back),
+              ),
+              Expanded(
+                child: Center(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Sign In',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 32, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do',
+                          textAlign: TextAlign.center,
+                          style:
+                              TextStyle(color: Colors.grey[600], fontSize: 14),
+                        ),
+                        SizedBox(height: 40),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Enter Email',
+                            hintText: 'youremail@gmail.com',
+                            hintStyle: TextStyle(
+                              color: Colors.grey.withOpacity(0.7),
+                            ),
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            suffixIcon: Icon(Icons.visibility_off),
+                            border: OutlineInputBorder(),
+                          ),
+                          obscureText: true,
+                        ),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text('Forget Password?'),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 150,
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.symmetric(vertical: 15),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              elevation: 5,
+                            ),
+                            child: Text(
+                              "Sign In",
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          children: [
+                            Expanded(child: Divider(thickness: 1)),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: Text('or sign in with'),
+                            ),
+                            Expanded(child: Divider(thickness: 1)),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.g_mobiledata, size: 40),
+                            ),
+                            IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.facebook, size: 40),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Don't have an account? "),
+                            GestureDetector(
+                              onTap: () {},
+                              child: Text(
+                                'Sign Up Now',
+                                style: TextStyle(color: Colors.blue),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
